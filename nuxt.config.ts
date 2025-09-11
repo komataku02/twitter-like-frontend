cd1b85c6c411d87afe426783a0a106ba08575adb
komachan <tkkm02@gmail.com>
2025-09-11 17:28:15 +0900

    chore: set srcDir=app and prepare firebase runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 
 export default defineNuxtConfig({
+  srcDir: 'app',
   pages: true,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
   plugins: ['~/plugins/axios'],
+  runtimeConfig: {
+    public: {
+      firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+    },
+  },
 })
